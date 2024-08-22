9e55bf8842a2c99ce0c15d5e08e8b5c24ca79954
Jagadish Krishnamoorthy <jagdish.krishna@gmail.com>
2024-08-22 16:59:57 -0700

    Update memory_stats.py

--- memory_stats.py
+++ memory_stats.py
@@ -3,4 +3,12 @@ import torch
 print("torch.cuda.memory_allocated: %fGB"%(torch.cuda.memory_allocated(0)/1024/1024/1024))
 print("torch.cuda.memory_reserved: %fGB"%(torch.cuda.memory_reserved(0)/1024/1024/1024))
 print("torch.cuda.max_memory_reserved: %fGB"%(torch.cuda.max_memory_reserved(0)/1024/1024/1024))
-print("torch.cuda.memory.mem_get_info: %fGB"%(torch.cuda.memory.mem_get_info('cuda:0')[0]/1024/1024/1024))
+print("torch.cuda.memory.mem_get_info free: %fGB"%(torch.cuda.memory.mem_get_info()[0]/1024/1024/1024))
+print("torch.cuda.memory.mem_get_info total: %fGB"%(torch.cuda.memory.mem_get_info()[1]/1024/1024/1024))
+
+# o/p on mi250 without any load
+torch.cuda.memory_allocated: 0.000000GB
+torch.cuda.memory_reserved: 0.000000GB
+torch.cuda.max_memory_reserved: 0.000000GB
+torch.cuda.memory.mem_get_info free: 63.787109GB
+torch.cuda.memory.mem_get_info total: 63.984375GB
