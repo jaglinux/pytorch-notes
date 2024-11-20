208442927a969af3273eb980cd8acabccfbdcd35
Jagadish Krishnamoorthy <jagdish.krishna@gmail.com>
2024-11-19 21:23:58 -0800

    Update cuda_device_count.py

--- cuda_device_count.py
+++ cuda_device_count.py
@@ -4,7 +4,7 @@ import torch
 
 # prints 2
 torch.cuda.device_count()
-#prints [0, 1] (gpu indices)
+#prints [0, 1] (gpu indices as per HIP_VISIBLE_DEVICES)
 torch.cuda._parse_visible_devices()
 
 # this prints the actual GPUs available.
