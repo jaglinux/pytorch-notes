055fa2daae3c6b2abbee604c67ef2229462c5658
Jagadish Krishnamoorthy <jagdish.krishna@gmail.com>
2024-11-25 12:29:52 -0800

    Update cuda_device.py

--- cuda_device.py
+++ cuda_device.py
@@ -12,3 +12,8 @@ True
 0
 AMD Instinct MI210
 '''
+print(torch.cuda.get_device_properties(0))
+'''
+_CudaDeviceProperties(name='AMD Instinct MI250X/MI250', major=9, minor=0, gcnArchName='gfx90a', 
+total_memory=65520MB, multi_processor_count=110, L2_cache_size=8MB)
+'''
